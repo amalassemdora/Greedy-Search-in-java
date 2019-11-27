66769f0171e145ecffe8c0ae70c7f96360cf645c
Amal Assem <amalassem21@gmail.com>
2019-11-27 19:17:44 +0200

    Initial Commit

---
+++ src/GreadySearch/Node.java
@@ -0,0 +1,41 @@
+/*
+*Programmer : Amal Assem Dora .
+*experience :2 Years in Java programming language .
+*Education : fourth year in computer science departement in faculty of electronic engineering (2019/2020).
+*Gready search code
+ */
+package GreadySearch;
+
+public class Node {
+
+    public Node parent;
+    public int[][] matrix;
+
+    // Blank tile cordinates
+    public int x, y;
+
+    // Number of misplaced tiles
+    public int cost;
+
+    // The number of moves so far
+    public int level;
+
+    public Node(int[][] matrix, int x, int y, int newX, int newY, int level, Node parent) {
+        this.parent = parent;
+        this.matrix = new int[matrix.length][];
+        for (int i = 0; i < matrix.length; i++) {
+            this.matrix[i] = matrix[i].clone();
+        }
+
+        // Swap value
+        this.matrix[x][y] = this.matrix[x][y] + this.matrix[newX][newY];
+        this.matrix[newX][newY] = this.matrix[x][y] - this.matrix[newX][newY];
+        this.matrix[x][y] = this.matrix[x][y] - this.matrix[newX][newY];
+
+        this.cost = Integer.MAX_VALUE;
+        this.level = level;
+        this.x = newX;
+        this.y = newY;
+    }
+
+}
